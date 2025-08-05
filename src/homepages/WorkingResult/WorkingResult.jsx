3b1e28d38d4abec64627a43b33727b4c6af39570
mhneloy <mhneloy708@gmail.com>
2025-08-05 19:43:50 +0600

    implement the get api for workingResult component

--- src/homepages/WorkingResult/WorkingResult.jsx
+++ src/homepages/WorkingResult/WorkingResult.jsx
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { useEffect } from "react";
 
 const WorkingResult = () => {
   const {
     data: allWork,
     isFetching: ispending,
-    error,
     refetch,
   } = useQuery({
     queryKey: ["allWork"],
@@ -14,12 +14,36 @@ const WorkingResult = () => {
       return res.data;
     },
   });
-  console.log(allWork);
+
+  useEffect(() => {
+    refetch();
+    console.log(allWork);
+  }, [refetch, allWork]);
+
+  if (ispending) {
+    return <span className="loading loading-spinner text-secondary"></span>;
+  }
+
   return (
     <div>
       <h3 className="text-center text-5xl text-brand font-bold my-8">
         Working Progress
       </h3>
+      <div>
+        {allWork.map((work, idx) => {
+          return (
+            <div key={idx} className="card bg-base-100 w-96 shadow-sm">
+              <figure>
+                <img src={work.photo} alt={work.name} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{work.name}</h2>
+                <p>{work.description}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
